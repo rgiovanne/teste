1eedd3bfd1099cfd1eb776e5c335909c5621bbb0
Giovanne <giovanne.ribeiro@hotmail.com>
2020-01-23 14:18:35 -0300

    primeiro commit

---
+++ src/components/Citacao.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="citacao">
+    <slot name= "texto"></slot>
+    <slot name= "fonte"></slot>
+    <div class="autor"><slot name= "autor"></slot></div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+props: ['texto']
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    h1 {
+        color: cyan;
+    }
+    .autor{
+        float: right;
+    }
+
+</style>
